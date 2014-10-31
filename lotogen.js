fbc709a48d432ae235d5f9cf130fe9164e64cc4a
javascriptpotato <javascriptpotato@gmail.com>
2014-10-31 11:59:29 +0200

    sync with local repo

--- lotogen.js
+++ lotogen.js
@@ -1,19 +1,28 @@
-var allLotoNumbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49];
+function loto(count, min, max) {
 
+    var arr = [];
 
-function getRandomNumbers(allLotoNumbers) {
+    for (var i = 0; i < count; i++) {
 
-for (var i = 0; i<6;i++) {
+        do {
+            var newNum = getInRange(min, max);
+        }
+        while (arr.indexOf(newNum) !== -1)
+        arr.push(newNum)
 
-
-var randomSixNumbers =  allLotoNumbers[Math.floor(Math.random()*allLotoNumbers.length)];
-document.write(randomSixNumbers);
-document.write("<br/>");
+    }
+    return arr;
 }
 
+function getInRange(min, max) {
+    return Math.floor(Math.random() * (max-min))+min;
 }
 
-getRandomNumbers(allLotoNumbers);
 
 
-// condition for duplicate numbers
+
+document.write(loto(6, 1, 49));
+
+
+
+
